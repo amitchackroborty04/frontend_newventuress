1645c6e5a7676d07ed77f5b0dd7dc68f8a0342d0
Monir Hossain Abde Rabby <monirhrabby.personal@gmail.com>
2025-02-13 11:47:42 +0600

    a

--- src/app/(website)/(auth)/registration/(a)/layout.tsx
+++ src/app/(website)/(auth)/registration/(a)/layout.tsx
@@ -4,7 +4,9 @@ import AuthUIProvider from "../../_components/provider/AuthUIProvider";
 export default function ALayout({ children }: { children: ReactNode }) {
   return (
     <AuthUIProvider sidebarImage="https://i.postimg.cc/QCCySSfp/image-9.png">
+      <div className="py-[30px]">
       {children}
+      </div>
     </AuthUIProvider>
   );
 }
--- src/app/(website)/(auth)/registration/country/[state]/_components/State-Header.tsx
+++ src/app/(website)/(auth)/registration/country/[state]/_components/State-Header.tsx
@@ -1,6 +1,6 @@
 const StateHeader = ({ country }: { country: string }) => (
   <div className="space-y-4 mb-[30px] text-left">
-    <h1 className="text-gradient text-3xl font-bold">
+    <h1 className="text-gradient text-2xl md:text-3xl font-bold">
       Select Any State of {country}
     </h1>
     <p className="text-gray-500 text-sm">
